7872741aaa6cd77b7708557689fc4b8bb90b9517
Tiago Sathler <sathler@gmail.com>
2023-10-26 21:23:47 -0300

    ✨ feat: instantiates Mp4Demuxer and VideoProcessor

--- initial-template/app/pages/file-upload/src/worker/worker.js
+++ initial-template/app/pages/file-upload/src/worker/worker.js
@@ -1,5 +1,47 @@
-onmessage = ({ data }) => {
-	setTimeout(() => {
-		self.postMessage({ status: "done" });
-	}, 2000);
+import VideoProcessor from "./videoProcessor.js";
+import MP4Demuxer from "./mp4Demuxer.js";
+
+const qvgaConstraints = {
+  width: 320,
+  height: 240,
+};
+
+const vgaConstraints = {
+  width: 640,
+  height: 480,
+};
+
+const hdConstraints = {
+  width: 1280,
+  height: 720,
+};
+
+const encoderConfig = {
+  ...qvgaConstraints,
+  bitrate: 10e6,
+  // WebM
+  codec: "vp09.00.10.08",
+  pt: 4,
+  hardwareAcceleration: "prefer-software",
+
+  // MP4
+  // codec: 'avc1.42002A',
+  // pt: 1,
+  // hardwareAcceleration: 'prefer-hardware',
+  // avc: { format: 'annexb' }
+};
+
+const mp4Demuxer = new MP4Demuxer();
+const videoProcessor = new VideoProcessor({ mp4Demuxer });
+
+onmessage = async ({ data }) => {
+  await videoProcessor.start({
+    file: data.file,
+    encoderConfig,
+    sendMessage(message) {
+      self.postMessage(message);
+    },
+  });
+
+  self.postMessage({ status: "done" });
 };
