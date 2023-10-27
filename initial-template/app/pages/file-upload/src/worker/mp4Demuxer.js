706134550bafa135d1285fdddb82d3aee29c8b85
Tiago Sathler <sathler@gmail.com>
2023-10-27 20:20:21 -0300

    ✨ feat: finishes MP4Demuxer

--- initial-template/app/pages/file-upload/src/worker/mp4Demuxer.js
+++ initial-template/app/pages/file-upload/src/worker/mp4Demuxer.js
@@ -1,4 +1,4 @@
-import { createFile } from "../deps/mp4box.0.5.2.js";
+import { createFile, DataStream } from "../deps/mp4box.0.5.2.js";
 
 export default class MP4Demuxer {
   #onConfig;
@@ -18,36 +18,79 @@ export default class MP4Demuxer {
   async run(stream, { onConfig, onChunk }) {
     this.#onConfig = onConfig;
     this.#onChunk = onChunk;
+
     this.#file = createFile();
 
-    this.#file.onReady = (args) => {
-      debugger;
-    };
+    this.#file.onReady = this.#onReady.bind(this);
+    this.#file.onSamples = this.#onSamples.bind(this);
 
     this.#file.onError = (err) => {
       console.error("mp4Demuxer error: ", err);
     };
 
-    this.#init(stream);
+    return this.#init(stream);
+  }
+
+  #onReady(info) {
+    const [track] = info.videoTracks;
+    this.#onConfig({
+      codec: track.codec,
+      codedHeight: track.video.height,
+      codedWidth: track.video.width,
+      description: this.#description(track),
+      durationSecs: info.duration / info.timescale,
+    });
+
+    this.#file.setExtractionOptions(track.id);
+    this.#file.start();
+  }
+
+  #onSamples(track_id, ref, samples) {
+    // Generate and emit an EncodedVideoChunk for each demuxed sample.
+    for (const sample of samples) {
+      this.#onChunk(
+        new EncodedVideoChunk({
+          type: sample.is_sync ? "key" : "delta",
+          timestamp: (1e6 * sample.cts) / sample.timescale,
+          duration: (1e6 * sample.duration) / sample.timescale,
+          data: sample.data,
+        })
+      );
+    }
+  }
+
+  #description({ id }) {
+    const track = this.#file.getTrackById(id);
+    for (const entry of track.mdia.minf.stbl.stsd.entries) {
+      const box = entry.avcC || entry.hvcC || entry.vpcC || entry.av1C;
+      if (box) {
+        const stream = new DataStream(undefined, 0, DataStream.BIG_ENDIAN);
+        box.write(stream);
+        return new Uint8Array(stream.buffer, 8);
+      }
+    }
+    throw new Error("avcC, hvcC, vpcC, or av1C box not found");
   }
 
   /**
-   *
    * @param {ReadableStream} stream
-   *
    * @returns Promise<void>
-   *
    */
   #init(stream) {
+    let _offset = 0;
     const consumeFile = new WritableStream({
       /**
        * @param {Uint8Array} chunk
        */
       write: (chunk) => {
-        debugger;
+        const { buffer } = chunk;
+        buffer.fileStart = _offset;
+        this.#file.appendBuffer(buffer);
+
+        _offset += chunk.length;
       },
       close: () => {
-        debugger;
+        this.#file.flush();
       },
     });
 
