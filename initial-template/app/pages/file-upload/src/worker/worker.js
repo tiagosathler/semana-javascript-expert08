dff2619ddea27a673da800dc7e42b1994b7f04c1
Tiago Sathler <sathler@gmail.com>
2023-10-27 20:22:26 -0300

    ✨ feat: invokes CanvasRenderer in worker.js

--- initial-template/app/pages/file-upload/src/worker/worker.js
+++ initial-template/app/pages/file-upload/src/worker/worker.js
@@ -1,5 +1,6 @@
 import VideoProcessor from "./videoProcessor.js";
 import MP4Demuxer from "./mp4Demuxer.js";
+import CanvasRenderer from "./canvasRenderer.js";
 
 const qvgaConstraints = {
   width: 320,
@@ -35,12 +36,12 @@ const mp4Demuxer = new MP4Demuxer();
 const videoProcessor = new VideoProcessor({ mp4Demuxer });
 
 onmessage = async ({ data }) => {
+  const renderFrame = CanvasRenderer.getRenderer(data.canvas);
+
   await videoProcessor.start({
     file: data.file,
+    renderFrame,
     encoderConfig,
-    sendMessage(message) {
-      self.postMessage(message);
-    },
   });
 
   self.postMessage({ status: "done" });
