4c96852185113ebd30e2fafc5371290a2c6c541e
Tiago Sathler <sathler@gmail.com>
2023-10-26 21:14:45 -0300

    🐛 fix: change to response.blob(), worker.error

--- initial-template/app/pages/file-upload/src/app.js
+++ initial-template/app/pages/file-upload/src/app.js
@@ -8,6 +8,10 @@ const worker = new Worker("./src/worker/worker.js", {
   type: "module",
 });
 
+worker.onerror = (error) => {
+  console.error("error worker", error);
+};
+
 worker.onmessage = ({ data }) => {
   if (data.status !== "done") return;
   clock.stop();
@@ -27,11 +31,13 @@ view.configureOnFileChange((file) => {
 
 async function fakeFetch() {
   const filePath = "/videos/frag_bunny.mp4";
-  const response = await fetch(filePath, { method: "HEAD" });
-  const file = new File([await response.blob], filePath, {
+  const response = await fetch(filePath);
+
+  const file = new File([await response.blob()], filePath, {
     type: "video/mp4",
     lastModified: Date.now(),
   });
+
   const event = new Event("change");
   Reflect.defineProperty(event, "target", { value: { files: [file] } });
   document.getElementById("fileUpload").dispatchEvent(event);
